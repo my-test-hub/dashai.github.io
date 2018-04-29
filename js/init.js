e785bdfc93316dab13d2b27d518b54f0866bc310
Tim Fairweather <tim.fairweather@arctiq.ca>
2018-04-29 10:38:16 -0400

    mobile nav working

--- js/init.js
+++ js/init.js
@@ -1,8 +1,15 @@
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $('.fade').slick({
+      dots: true,
+      arrows: true,
+      infinite: true,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
